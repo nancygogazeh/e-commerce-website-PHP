471b455c2387977a3c022905cde13746a808e893
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-09-30 17:58:49 +0300

    shop initial design front-end

---
+++ store.php
@@ -0,0 +1,129 @@
+<?php
+include "./includes/header.php";
+?>
+<div class="container-fluid bg-light">
+    <div class="row pt-5">
+        <div class="col-3">
+            <div class="row g-0">
+                <div class="col-12">
+                    <form action="" method="get" class="d-flex gap-2 ">
+                        <input type="text" name="" id="" placeholder="Search products.." class="w-100">
+                        <button type="submit" class="btn">
+                            <i class="bi bi-caret-right-fill"></i>
+                        </button>
+                    </form>
+                </div>
+                <div class="col-12 mt-5">
+                    <h3>Filter by price</h3>
+
+                </div>
+
+                <div class="col-12 mt-5 pb-5">
+                    <h3 class="pb-4 pt-2 bg-white mb-0">Categories</h3>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-8">
+
+
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3">A JACK AND THREEA JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                            <div class="price">$99.9 </div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3"> JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3"> JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+                </div>
+
+
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3"> JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+                </div>
+
+
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3"> JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 justify-content-between">
+                        <div class="text-center">
+                            <img src="https://i.imgur.com/TbtwkyW.jpg" width="250">
+                        </div>
+                        <div class="text-center">
+                            <h5 class="px-2 py-3"> JACK AND THREE QUEENS QUEENSA JACK AND THREE QUEENS</h5>
+                            <div class="my-2 fs-4">category</div>
+                        </div>
+                        <div class="cart-btn w-100 price">ADD TO CART</div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+        <?php
+        include "./includes/footer.php";
+        ?>
