a079ab2531de649b7cfe85b8ece9c936b53fcabd
nancy <nancy.agogazeh@gmail.com>
2022-10-01 14:40:44 +0300

    new

--- nav.php
+++ nav.php
@@ -1,30 +1,140 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>navbar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/includes/bootstrap-5.2.1-dist/css/styl.css">
+  <script src="bootstrap-5.2.1-dist/js/index.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+
+</head>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
   <div class="container-fluid">
-    <img src= "./image/bookstore.png" style="width:30px"; height="30px" ;> 
-    <a class="navbar-brand" href="#" style= "color:black;">Baghdad</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Navbar brand -->
+      <a class="navbar-brand mt-2 mt-lg-0" href="#">
+        <img class="rounded-circle"
+            height="35"
+          src="./image/bookstore.png"
+          height="15"
+          alt="book Logo"
+          loading="lazy"
+        />
+      </a>
+      <!-- Left links -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#" style="color:black ">Home</a>
+          <a class="nav-link" href="#">Baghdad</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style="color:black ">Shop</a>
+          <a class="nav-link" href="#">Shop</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style="color:black ">Category</a>
+          <a class="nav-link" href="#">Category</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact-us</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-</svg>
-
+      <!-- Left links -->
     </div>
+    <!-- Collapsible wrapper -->
+
+    <!-- Right elements -->
+    <div class="d-flex align-items-center">
+      <!-- Icon -->
+      <a class="text-reset me-3" href="#">
+        <i class="fas fa-shopping-cart"></i>
+      </a>
+
+      <!-- Notifications -->
+      <div class="dropdown">
+        <a
+          class="text-reset me-3 dropdown-toggle hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuLink"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="fas fa-bell"></i>
+          <span class="badge rounded-pill badge-notification bg-danger"><img class="rounded-circle"
+            height="25" src="./image/icon.png"> </span>
+        </a>
+        <ul
+          class="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuLink"
+        >
+          <li>
+            <a class="dropdown-item" href="#">Some news</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Another news</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </li>
+        </ul>
+      </div>
+      <!-- Avatar -->
+      <div class="dropdown">
+        <a
+          class="dropdown-toggle d-flex align-items-center hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuAvatar"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <img
+            src="./image/user.png"
+            class="rounded-circle"
+            height="25"
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+          />
+        </a>
+        <ul
+          class="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuAvatar"
+        >
+          <li>
+            <a class="dropdown-item" href="#">My profile</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Settings</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- Right elements -->
   </div>
+  <!-- Container wrapper -->
 </nav>
+<!-- Navbar -->
--- register.php
+++ register.php
@@ -1,9 +1,136 @@
-<?php 
-include "./includes/header.php";
 
+<?php 
+    include "../includes/connection.php";
+    include "./includes/header.php";
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Book Store</title>
 
+    <link href="../css/style.css" rel="stylesheet">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script>
+        function pass()
+        {
+            var firstpass = document.form.pass.value;
+            var secondpass = document.form.cpass.value;
+            if(firstpass == secondpass)
+            {
+                return true;
+            }
+            else
+            {
+                alert("Passwords doesn't match.\n Please check and Re-Enter the password");
+                return false;
+            }
+        }
+    </script>
+</head>
+<body>
+    <section id="registerHome">
+        <div class="registerContainer">
+        <div class="container">
+            <div class="Title"><h2>Register</h2></div>
+                <form class="registerForm" action="" method="POST">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="fname" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter your First Name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="lname" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" id="lname" name="lname" placeholder="Enter your Last Name" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter Email Address" aria-describedby="emailHelp" pattern="[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+" title="Example: name@email.com" required>
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" id="password" class="form-control" name="pass" placeholder="Your Password" pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$" title="One Uppercase, One Lowercase, One special Character, at least 8 digits" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cpassword" class="form-label">Password</label>
+                        <input type="password" id="cpassword" class="form-control" name="cpass" placeholder="Confirm Password" required>
+                    </div>
+                    <button onclick="pass()" type="submit" class="btn btn-primary">Register</button>
+                    <a class="btn btn-secondary" href="login.php">Login</a>
+                </form>
+            </div>
+        </div>
+        <?php
+            if(isset($_POST['email'])) 
+            {
+                $username = $_POST['username'];
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                $pass = $_POST['pass'];
+                $role = "user";
+            }
+
+            if(mysqli_connect_error())
+            {
+                die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+            }
+            else
+            {
+                $SELECT = "SELECT email FROM register WHERE email = ? LIMIT 1";
+                $INSERT = "INSERT INTO register (username, firstname, lastname, email, password, role) VALUES (?, ?, ?, ?, ?, ?)";
+            }
+
+            // Prepare Statement
+            $stmt = $conn->prepare($SELECT);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($email);
+            $stmt->store_result();
+            $rnum = $stmt->num_rows;
+
+            if($rnum==0)
+            {
+                $stmt->close();
+
+                $stmt = $conn->prepare($INSERT);
+                $stmt->bind_param("ssssss", $username, $fname, $lname, $email, $pass, $role);
+                $stmt->execute();
+                
+                ?>
+                <script>
+                    window.alert("Registered Successfully");
+                    window.location.href = "login.php";
+                </script>
+                <?php
+            }
+            
+            else 
+            {
+                ?>
+                <script>
+                    window.alert("Someone already have with this email, please Try an different email");
+                    setTimeout(function(){ window.location.href = "register.php"; }, 2000);           
+                </script>
+
+                <?php
+            } 
+            $stmt->close();
+            $conn->close();
+        ?>
+    </section>
+</body>
+</html>
 
 
 
