c626defe6803705080fc09af4d195a4ffa229922
nancy <nancy.agogazeh@gmail.com>
2022-10-01 23:04:13 +0300

    new responsive

--- home.php
+++ home.php
@@ -1,21 +1,32 @@
-
 <?php 
 include "./includes/header.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="includes/bootstrap-5.2.1-dist/css/style.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+<body>
+    
 
 <div >
-  <img src="./image/1.jpg" alt="Snow" style="width:100%; height:600px;">
+  <img src="./image/1.jpg" alt="Snow" style="width:100%; height:500px;">
   
   <h1 id="toplft"class="top-lft" >Best Books <br> delivered at your  </br>doorsteps <br/> <button class= "btn btn-light" style="width:150px ; height:45px;"> Shop Now</button>
 </h1>
-
-  
-  <br/><br>
+<br/> <br/> 
 </div>
-
-    <div class="container">
+<br/><div class="container">
         <h2>Our  Partners/ Our Clients</h2>
+        <hr>
          <section class="customer-logos slider">
             <div class="slide"><img src="./image/imageedit_7_4955298552.png"></div>
             <div class="slide"><img src="./image/imageedit_4_8801715882.png"></div>
@@ -27,20 +38,17 @@ include "./includes/header.php";
             <div class="slide"><img src="./image/imageedit_4_8801715882.png"></div>
             <div class="slide"><img src="./image/imageedit_2_3303768801.png"></div>
          </section>
-         
-     
-      </div>
-     <br/>
-     <br/><br/>
 
-      <br/><div class="container">
+      </div>
+      <br/><br/>
+ 
   <!-- Content here -->
 
   <div class="container text-center">
   <div class="row">
     <div class="col">
     <div >
-  <img src="./image/first-book.jpg" alt="adult" style="width:250px; height:250px;opacity: 0.8;">
+  <img src="./image/first-book.jpg" alt="adult"  style="width:250px; height:250px;opacity: 0.9;">
   
   <p class="centered">20% Off on Adults <br> Books  </br>Use adult 20 to get discount on your book<button class= "btn btn-light"> Shop Now</button>
 </p>
@@ -81,16 +89,16 @@ include "./includes/header.php";
 <br/>
 <br/>
 
-<br/><div class="container">
+<br/><div class="container book">
   <!-- Content here -->
   <br/><br/>
-<h1> FEATURED BOOKS</h1><br/><br/>
-<p>-------------------</p>
+<h1 class="text-center "> FEATURED BOOKS</h1><br/><br/>
+<hr>
 <br/><br/><br/>
 <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card h-100">
-      <img src="./image/2.jpg" class="card-img-top" alt="...">
+      <img src="https://via.placeholder.com/250C/O https://placeholder.com/" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Big Bad</h5>
         <p class="card-text">Thrillel</p>
@@ -114,7 +122,7 @@ include "./includes/header.php";
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="./image/3.jpg" class="card-img-top" alt="...">
+      <img src="https://via.placeholder.com/250C/O https://placeholder.com/" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Nona the Ninth
 </h5>
@@ -137,7 +145,7 @@ include "./includes/header.php";
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="./image/41EscppUr0L._SX327_BO1204203200_.jpg" class="card-img-top" alt="Thrillel">
+      <img src="https://via.placeholder.com/250C/O https://placeholder.com/" class="card-img-top" alt="Thrillel">
       <div class="card-body">
         <h5 class="card-title">The Atlas Six </h5>
         <p class="card-text">Mystery</p>
@@ -161,7 +169,7 @@ include "./includes/header.php";
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="./image/7.jpg" class="card-img-top" alt="...">
+      <img src="https://via.placeholder.com/250C/O https://placeholder.com/" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Where
              Grows</h5>
@@ -184,7 +192,7 @@ include "./includes/header.php";
   </div>
   <div class="col">
     <div class="card h-100">
-      <img src="./image/6.jpg" class="card-img-top" alt="...">
+      <img src="https://via.placeholder.com/250C/O https://placeholder.com/" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Don't Look
 Back</h5>
@@ -306,7 +314,7 @@ Back</h5>
 
 
 <script>
-    $(document).ready(function(){
+ $(document).ready(function(){
     $('.customer-logos').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
